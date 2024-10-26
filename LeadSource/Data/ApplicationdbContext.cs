a32b175d408f7fbca46a2a4b2a3a4c884ce243a3
gurumalleshb7760 <gurugannu2001@gmail.com>
2024-10-26 13:46:56 +0530

    Add project files.

---
+++ LeadSource/Data/ApplicationdbContext.cs
@@ -0,0 +1,14 @@
+﻿using LeadSource.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LeadSource.Data
+{
+    public class ApplicationdbContext : DbContext
+    {
+        public ApplicationdbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<LeadEntity> Leads { get; set; }
+    }
+}
